231fcf1a0620407c368826e39cede53ddfa486fd
Leonardo Borgo <borgoleonardo5@gmail.com>
2025-03-13 15:41:35 -0300

    Quinto commit, con ej de numeros float

---
+++ git.sh
@@ -0,0 +1,8 @@
+git config --global --unset user.name 
+git config --global --unset user.email
+git config --global --unset user.password
+git config --global --unset credential.helper
+cmdkey /delete:LegacyGeneric:target=git:https://github.com
+git config --global --unset user.name "Leonardo Borgo"
+git config --global --unset user.email "borgoleonardo5@gmail.com"
+giy config --gloabl --unset user.password "LeONARDO22..Mko"
